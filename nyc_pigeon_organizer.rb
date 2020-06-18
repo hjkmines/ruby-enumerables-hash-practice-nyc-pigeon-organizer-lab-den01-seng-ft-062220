5fbe0146dbb898e8b1e2112fdebe5f85350be520
Hyung Kim <hjkmines@gmail.com>
2020-06-18 16:17:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-  data[:gender][:male]
-  binding.pry 
-  data[:gender][:female]
+  males = data[:gender][:male]
+  females = data[:gender][:female]
   
   
   
