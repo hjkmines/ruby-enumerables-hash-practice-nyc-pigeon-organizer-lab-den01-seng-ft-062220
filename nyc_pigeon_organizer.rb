a6221a6b1fe74d6781aa0534ab2ac972b467e273
Hyung Kim <hjkmines@gmail.com>
2020-06-18 17:09:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,24 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
-  final_results = data.each_with_object ({}) do |(key, value), final_array|
-
-    value.each do |inner_key, names|
-      names.each do |name|
-       if !final_array[name]
-        final_array[name] = {}
-       end
-       if !final_array[name][key]
-        !final_array[name][key] = []
-       end
-     
-    end
-  end
-end
-final_results
-binding.pry 
-puts ""
-end
+  new_hash = {}
+  
+  data.each do |key, inner_key|
+    
 
 
 
