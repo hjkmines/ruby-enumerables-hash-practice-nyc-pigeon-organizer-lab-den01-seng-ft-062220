66ba3c5e79aa6821a32806a53691394b610ba10b
Hyung Kim <hjkmines@gmail.com>
2020-06-18 17:13:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,14 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   
   data.each do |key, value|
-    value.each do |inner_key, inner
-
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][inner_key]
+            new_hash[name][inner_key] = []
+        end 
 
 
 
