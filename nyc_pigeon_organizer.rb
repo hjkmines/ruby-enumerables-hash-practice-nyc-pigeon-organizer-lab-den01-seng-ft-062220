b2d361fc3135dad888d0b65a737865b2396ab2bc
Hyung Kim <hjkmines@gmail.com>
2020-06-18 16:19:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,10 @@ def nyc_pigeon_organizer(data)
   males = data[:gender][:male]
   females = data[:gender][:female]
   
+  combined_gender = males.concat(females)
+  binding.pry 
   
+  final_output = {}
   
 end 
 
